6f067231fc80e37cbcf9f17170a48228d18ca847
yoshiyoshiharu <haruki.osaka.u@gmail.com>
2023-05-06 19:04:51 +0900

    refactor

--- gls/current_dir.go
+++ gls/current_dir.go
@@ -4,22 +4,13 @@ import (
 	"os"
 )
 
-func DirFiles(dir string) ([]os.DirEntry, error) {
-	fileInfos, err := os.ReadDir(dir)
-	if err != nil {
-		return nil, err
-	}
-
-	return fileInfos, nil
-}
-
 func CurrentDirFiles() ([]os.DirEntry, error) {
-	currentDir, err := os.Getwd()
+	dir, err := os.Getwd()
 	if err != nil {
 		return nil, err
 	}
 
-	files, err := DirFiles(currentDir)
+	files, err := os.ReadDir(dir)
 	if err != nil {
 		return nil, err
 	}
