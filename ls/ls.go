f0bca16a98c33f6921a540136cd1b33f2311bc36
yoshiyoshiharu <haruki.osaka.u@gmail.com>
2023-05-06 13:45:58 +0900

    move ls.go

---
+++ ls/ls.go
@@ -0,0 +1,37 @@
+package ls
+
+import (
+	"os"
+)
+
+func currentDir() (string, error) {
+	dir, err := os.Getwd()
+	if err != nil {
+		return "", err
+	}
+
+	return dir, nil
+}
+
+func DirFiles(dir string) ([]os.DirEntry, error) {
+	fileInfos, err := os.ReadDir(dir)
+	if err != nil {
+		return nil, err
+	}
+
+	return fileInfos, nil
+}
+
+func CurrentDirFiles() ([]os.DirEntry, error) {
+	currentDir, err := currentDir()
+	if err != nil {
+		return nil, err
+	}
+
+	files, err := DirFiles(currentDir)
+	if err != nil {
+		return nil, err
+	}
+
+	return files, nil
+}
